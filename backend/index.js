ea821dd47fe0951d456b39008dcc8d4fe65b2c69
dhanraj644 <malidhanraj644@gmail.com>
2024-08-10 22:42:36 +0530

    product.schema.model

--- backend/index.js
+++ backend/index.js
@@ -27,11 +27,64 @@ app.use(cors())
 // Creating upload  Endpoint for image
     app.use('/images',express.static('upload/images'))
     
-    app.post("/upload",upload.single('product'),(req,res)=>{
+    app.post("/upload",upload.single('Product'),(req,res)=>{
         res.json({
             success:1,
             image_url:`http://localhost:${port}/images/${req.file.filename}`
         })
+    })
+  
+// database model
+    const Product=mongoose.model("Product",{
+        id:{
+            type:Number,
+            require:true,
+        },
+        name:{
+            type:String,
+            require:true,
+        },
+        image:{
+            type:String,
+            require:true,
+        },
+        category:{
+            type:String,
+            require:true,
+        },
+        new_price:{
+            type:Number,
+            require:true,
+        },
+        old_price:{
+            type:Number,
+            require:true,
+        },
+        date:{
+            type:Date,
+            default:Date.now,
+        },
+        available:{
+            type:Boolean,
+            default:true,
+        },
+
+
+    })    
+
+
+
+    app.post("/addproduct",async (req,res)=>{
+        const product=new Product({
+            id:req.body.id,
+            name:req.body.name,
+            image:req.body.image,
+            catogary:req.body.category,
+            new_price:req.body.new_price,
+            old_price:req.body.old_price,
+
+        })
+        await product.save()
     })
 
 // Api creation
