950692f51b1dfce4aa588a35b9c94793d6d752eb
dhanraj644 <malidhanraj644@gmail.com>
2024-08-14 23:13:44 +0530

    navbar-admin

--- admin/src/component/Navbar/Navbar.jsx
+++ admin/src/component/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        
+       <div className="logo">
+
+ <a href=""><h2>WEAR <span style={{color:'crimson'}}>IT</span></h2></a>
+</div>
     </div>
   )
 }
---
+++ admin/src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '/Sidebar.css'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+
+    </div>
+  )
+}
+
+export default Sidebar
