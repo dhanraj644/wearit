1c8bfe712b88ff21c6ae7d14dadbceecc6ee5a3c
dhanraj644 <malidhanraj644@gmail.com>
2024-08-14 23:00:11 +0530

    file-struct-admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ admin/src/Pages/admin/Admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Admin.css'
+
+ const Admin = () => {
+  return (
+    <div className='admin'>
+       </div>
+  )
+}
+
+ export default Admin
