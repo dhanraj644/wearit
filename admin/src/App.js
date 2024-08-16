552a4f1bc31e274237039f77ddae54da08b3b42d
dhanraj644 <malidhanraj644@gmail.com>
2024-08-16 23:01:18 +0530

    up--date

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import Admin from './Pages/admin/Admin'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Admin/>
     </div>
   );
 }
--- admin/src/component/Sidebar/Sidebar.jsx
+++ admin/src/component/Sidebar/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import '/Sidebar.css'
+import './Sidebar.css'
+import { Link } from 'react-router-dom'
+import add_product_icon from '../../assets/Product_Cart.svg'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
+      <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+      <div className="sidebar-item">
+        <img src={add_product_icon}  alt=''/>
+      </div>
+      </Link>
 
     </div>
   )
